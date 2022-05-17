e343858ee5ab09308ad36c64a8ef63d5a1edc517
user <cksqhr4961@naver.com>
2022-05-17 21:28:41 +0900

    "init"

---
+++ kakaoapi/src/main/java/login/kakaoapi/KakaoapiApplication.java
@@ -0,0 +1,13 @@
+package login.kakaoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoapiApplication.class, args);
+	}
+
+}
