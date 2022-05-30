f7d6b26121f861b05b697e0260974ac6ece7a414
user <cksqhr4961@naver.com>
2022-05-30 20:34:14 +0900

    init

---
+++ spring-start-mybatis/src/main/java/com/mybatis/SpringStartMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStartMybatisApplication.class, args);
+	}
+
+}
