895024dec5ebb6bc9fabae3299af28ac97b9f47c
chanbokW <82792464+chanbokW@users.noreply.github.com>
2022-06-01 15:21:01 +0900

    Delete study directory

--- study/src/main/java/com/example/study/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.study.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.study.domain.Member;
-
-public interface MemberRepository extends JpaRepository<Member, Long>{
-	
-}
